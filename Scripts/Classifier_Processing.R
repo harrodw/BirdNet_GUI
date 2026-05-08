8fefab1309cf403a3c5c8d01e1bde29848031b63
harrodw <will.harrod99@gmail.com>
2026-05-08 08:40:11 -0400

    Initial commit

---
+++ Scripts/Classifier_Processing.R
@@ -0,0 +1,181 @@
+# ################################################################################
+# Title: Making a BirdNET species list
+# BirdNet R workflow, Script 2 of ???
+# Author: Will Harrod
+# Date Created: 2026-05-01
+# Credit to  the list creator: 
+# https://github.com/conormcmahon/birdnet_species_list_processing/blob/main/all_species_metadata.csv
+################################################################################
+
+################################################################################
+# 1) Prep ######################################################################
+################################################################################
+
+# Add packages
+library(tidyverse)
+library(fs)
+
+# Define the working directory as the folder that holds the BirdNET csv's
+wd <- "C:/NCSU/Disertation_Code/BirdNet_GUI/Data"
+
+# Viw the folders in that working directory
+sites <- dir_ls(wd, regexp = "[A-Z]{2}\\d{2}")
+sites
+
+# How many sites
+n_sites <- length(sites)
+n_sites
+
+# View the contents of one folder
+bn_results_s1 <- dir_ls(sites[1], regexp = "results.csv")
+bn_results_s1
+
+# Read in the first output csv
+bn_results <- read.csv(bn_results_s1[1]) %>% 
+  tibble() %>% 
+  rename(Start.sec = Start..s.,
+         End.sec = End..s.) %>% 
+  mutate(across(.cols = c(Scientific.name, Common.name, File),
+                .fns = ~ as.character(NA))) %>% 
+  mutate(across(.cols = c(Start.sec, End.sec, Confidence),
+                .fns = ~ as.numeric(NA))) %>% 
+  slice_head(n = 1)
+
+# View
+bn_results
+glimpse(bn_results)
+
+# Loop through the ARU files to combine them all 
+for(i in 1:n_sites){
+
+  # Define an ARU
+  site <- sites[i]
+  
+  # List the summary csv's in that folder
+  class_sums <- dir_ls(site, regexp = "results.csv")
+  
+  # Count the number of csv's in that folder
+  n_csv <- length(class_sums)
+  
+  # Progress message 
+  message("Combinging classification summaries for aru ", i, 
+          " out of ", n_sites, ". ", n_csv, " recordings ")
+  
+  # Loop through these and combine them 
+  for(j in 1:n_csv){
+    
+    # Define a single summary
+    class_sum <- read.csv(class_sums[j]) %>% 
+      tibble() %>% 
+      rename(Start.sec = Start..s.,
+             End.sec = End..s.) %>% 
+      mutate(across(.cols = c(Scientific.name, Common.name, File),
+                    .fns = ~ as.character(.x))) %>% 
+      mutate(across(.cols = c(Start.sec, End.sec, Confidence),
+                    .fns = ~ as.numeric(.x))) 
+    
+    # Combine that with the other recording summaries
+    bn_results <- bind_rows(bn_results, class_sum)
+    
+  }
+}
+
+# View the full combined BirdNET classifier output
+glimpse(bn_results)
+slice_head(bn_results)
+
+# How many distinct species?
+bn_results %>% 
+  count(Scientific.name) %>% 
+  nrow()
+
+# Add in the possible species list 
+psb_species <- read.csv("Data/possible_species.csv") %>% 
+  tibble() %>% 
+  mutate(Scientific.name = paste(genus, species)) %>% 
+  rename(Common.name = common_name)
+# View
+glimpse(psb_species)
+
+# List of anthropogenic sounds
+anthro_sounds <- c("Engine", "Gun", "Human non-vocal", "Human vocal", "Power tools")
+
+# Combine with the Birdnet Results
+bn_results_taxon <- left_join(bn_results, psb_species, 
+                              by = c("Scientific.name", "Common.name")) %>% 
+  rename(Latin.name = Scientific.name) %>% 
+  mutate(class = case_when(Latin.name == "Dog" ~ "Mammalia",
+                           Latin.name %in% anthro_sounds ~ "Anthropogenic",
+                           TRUE ~ class),
+         order = case_when(Latin.name == "Dog" ~ "Carnivora",
+                           Latin.name %in% anthro_sounds ~ "Anthropogenic",
+                           TRUE ~ order))  %>% 
+  arrange(class, order, Common.name) %>% 
+  # Extract information from the file names
+  mutate(
+         Plot = factor(str_extract(File, pattern = "[A-Z]{2}\\d{2}")),
+         Date = ymd(str_extract(File, pattern = "\\d{8}")),
+         Time = str_extract(File, pattern = "\\d{6}.wav")
+         ) %>% 
+  mutate(
+         Time = str_remove_all(Time, pattern = ".wav"),       # Raw time
+         Plot.Type = factor(str_remove_all(Plot, "\\d{2}")) # Plot Type
+         ) %>% 
+  mutate(Rec.Hour = as.numeric(str_sub(Time, start = 1, end = 2)), # Start Hour
+         Rec.Min = as.numeric(str_sub(Time, start = 3, end = 4))   # Recording Minute
+         ) %>% 
+  # Reorder columns and remove unnessesary ones
+  select(
+         Plot, Plot.Type , Date, Rec.Hour, Rec.Min, Start.sec, End.sec,
+         Common.name, Confidence, Latin.name, class, order, File
+         )
+
+
+# View
+glimpse(bn_results_taxon)
+print(bn_results_taxon, n = 30)
+
+# Save the full results
+bn_results_taxon %>% write.csv("Data/preliminary_BirdNET_Results.csv")
+
+# Summarize by species
+species_sums <- bn_results_taxon %>% 
+group_by(class, order, Latin.name, Common.name) %>% 
+  reframe(Latin.name, Common.name, Count = n(), mean.Conf = mean(Confidence)) %>% 
+  distinct()
+  
+# View
+glimpse(species_sums)
+species_sums %>% 
+  arrange(-Count) %>% 
+  print(n = Inf)
+
+# Save the summary
+write.csv(species_sums, "Data/bn_naive_species_summary.csv")
+
+# Naive number of species by site
+site_sums <- bn_results_taxon %>% 
+  distinct(Plot, Plot.Type, Common.name) %>% 
+  group_by(Plot, Plot.Type) %>% 
+  reframe(Plot, Plot.Type, Species.Count = n()) %>% 
+  distinct()
+
+# View
+glimpse(site_sums)
+print(site_sums, n = Inf)
+
+# Plot of species by site
+site_sums %>% 
+  ggplot() +
+  geom_col(aes(x = Plot, y = Species.Count, col = Plot.Type, fill = Plot.Type)) +
+  theme_classic()
+
+# Plot of species by plot type
+site_sums %>% 
+  ggplot() +
+  geom_boxplot(aes(x = Plot.Type, y = Species.Count)) +
+  theme_classic()
+
+# View a specific species
+bn_results_taxon %>% 
+  filter(Common.name == "Green Frog")
---
+++ Scripts/Subsetting_Data.R
@@ -0,0 +1,164 @@
+################################################################################
+# Title: Subsetting BirdNET
+# BirdNet R workflow, Script 1 of ???
+# Author: Will Harrod
+# Date Created: 2026-05-01
+################################################################################
+
+################################################################################
+# 1) Preparation ###############################################################
+################################################################################
+
+# Clear environments
+rm(list = ls())
+
+# Load packages
+library(tidyverse)
+library(birdnetR)
+library(tuneR)
+library(fs)
+
+# Path to wd
+wd <- "D:/Bird_Anuran_ARUs"
+
+# Set wd
+setwd(wd)
+
+################################################################################
+# 2) Inspect data ##############################################################
+################################################################################
+
+# 2.1) Recordings by site ------------------------------------------------------
+
+# Find all the ARU folders
+aru_dirs <- list.dirs(wd)
+aru_dirs <- aru_dirs[2:length(aru_dirs)]
+
+# How many ARUs?
+length(aru_dirs)
+
+# View the ARUs
+aru_dirs
+
+# Find the number of recordings made by each ARU
+rec_counts <- aru_dirs %>% 
+  set_names() %>% 
+  map_int(~length(list.files(.x))) %>% 
+  enframe(name = "path", value = "rec.count") %>% 
+  mutate(site.id = str_extract(path, "[A-Z]{2}\\d{2}")) %>% 
+  select(path, site.id, rec.count)
+
+
+# View the counts by ARU
+print(rec_counts, n = Inf)
+
+# 2.2) days per recording ------------------------------------------------------
+
+# Add storage columns to the tibble
+rec_counts <- rec_counts %>% 
+  mutate(start.date = as.Date(NA),
+         end.date = as.Date(NA),
+         total.days = NA)
+
+# Loop through the files and count he first, last, and total days 
+for(i in 1:nrow(rec_counts)){
+
+  # Define an ARU
+  aru <- rec_counts$path[i]
+  
+  # List the recordings made by that ARU
+  recordings <- list.files(aru)
+  
+  # Isolate the days 
+  days <- str_extract(recordings, "_\\d{8}_") %>% 
+    str_remove_all("_") %>% 
+    ymd()
+  
+  # Find the first and last dates
+  rec_counts$start.date[i] <- ymd(min(days))
+  rec_counts$end.date[i] <- ymd(max(days))
+  rec_counts$total.days[i] <- max(days) - min(days) + 1
+  
+}
+
+# View the result
+print(rec_counts, n = Inf)
+
+# which recorder had the fewest days?
+min_days_dat <- rec_counts %>% 
+  filter(total.days == min(rec_counts$total.days)) 
+min_days_dat
+
+
+################################################################################
+# 3) Subset data ###############################################################
+################################################################################
+
+# Path to wd
+wd <- "D:"
+
+# Set wd
+setwd(wd)
+
+# List the minimum days 
+min_days <- min_days_dat$total.days
+min_days
+
+# Create a folder for subsetted data
+out_dir <- "D:/Subsetted_ARU_Data"
+dir_create(out_dir, recurse = TRUE)
+
+# How many ARUs?
+n_arus <- nrow(rec_counts)
+
+# Loop through the recordings and randomly sample based on the fewest number of days 
+for(i in 1:n_arus){ 
+
+  # Define an ARU
+  aru_path <- rec_counts$path[i]
+  aru <- rec_counts$site.id[i]
+  
+  # Create a new folder for that aru
+  dir_create(paste0("D:/Subsetted_ARU_Data/", aru), recurse = TRUE)
+  
+  # List the recordings made by that ARU
+  recordings <- tibble(audio.files = list.files(aru_path))
+  
+  # Isolate the days 
+  recordings <- recordings %>% 
+    mutate(day = str_extract(audio.files, "_\\d{8}_")) %>% 
+    mutate(day = str_remove_all(day, "_"))
+  
+  # Find the unique days 
+  unq_days <- recordings %>% 
+    distinct(day) 
+
+  # subset a few of days 
+  slct_days <- unq_days %>% 
+    slice_sample(n = min_days) %>% 
+    pull(day)
+  
+  # List the recordings that took place during those days
+  slct_recs <- recordings %>% 
+    filter(day %in% slct_days) %>% 
+    pull(audio.files)
+  
+  # List all of the files
+  all_wavs <- dir_ls(
+    path = aru_path,
+    recurse = TRUE,
+    regexp = "\\.[Ww][Aa][Vv]$",
+    type = "file"
+  )
+  
+  # Filter the ones that were selected 
+  slct_wavs <- all_wavs[basename(all_wavs) %in% slct_recs]
+  
+ # Copy those files 
+  file_copy(
+    path = slct_wavs,
+    new_path = path(paste0("D:/Subsetted_ARU_Data/", aru), basename(slct_wavs)),
+    overwrite = TRUE
+  )
+  
+} # End loop over the ARUs
